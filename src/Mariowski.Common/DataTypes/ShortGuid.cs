52f01fd1ba562e873b2f4bcac360fcd18a1c9756
Radek Mariowski <rmariowski@gmail.com>
2019-03-09 20:37:40 +0100

    ShortGuid

---
+++ src/Mariowski.Common/DataTypes/ShortGuid.cs
@@ -0,0 +1,264 @@
+﻿using System;
+
+namespace Mariowski.Common.DataTypes
+{
+    public struct ShortGuid
+    {
+        #region Fields
+
+        /// <summary>
+        /// A read-only instance of the <see cref="T:ShortGuid"></see> structure
+        /// whose guid is all zeros and value is null.
+        /// </summary>
+        public static readonly ShortGuid Empty = default(ShortGuid);
+
+        private Guid _guid;
+        public Guid Guid
+        {
+            get => _guid;
+            set
+            {
+                if (value == _guid)
+                    return;
+
+                _guid = value;
+                _value = Encode(value);
+            }
+        }
+
+        private string _value;
+        public string Value
+        {
+            get => _value;
+            set
+            {
+                if (value == _value)
+                    return;
+
+                _value = value;
+                _guid = Decode(value);
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a ShortGuid from a base64 encoded string.
+        /// </summary>
+        /// <param name="s">The encoded guid as a base64 string.</param>
+        /// <exception cref="T:System.ArgumentNullException"><paramref name="s">s</paramref> is null.</exception>
+        /// <exception cref="T:System.FormatException">The length of <paramref name="s">s</paramref>, ignoring white-space characters, is not zero or a multiple of 4.   -or-   The format of <paramref name="s">s</paramref> is invalid. <paramref name="s">s</paramref> contains a non-base-64 character, more than two padding characters, or a non-white space-character among the padding characters.</exception>
+        public ShortGuid(string s)
+        {
+            _guid = Decode(s);
+            _value = s;
+        }
+
+        /// <summary>
+        /// Creates a ShortGuid from a Guid.
+        /// </summary>
+        /// <param name="guid">The Guid to encode.</param>
+        public ShortGuid(Guid guid)
+        {
+            _value = Encode(guid);
+            _guid = guid;
+        }
+
+        #endregion
+
+        #region Equals
+
+        /// <summary>
+        /// Returns a value that indicates whether this instance is equal to a specified object.
+        /// </summary>
+        /// <param name="o">The object to compare with this instance.</param>
+        /// <returns>
+        /// true if <paramref name="o">o</paramref> is a <see cref="T:ShortGuid"></see> that has the same value as this instance;
+        /// true if <paramref name="o">o</paramref> is a <see cref="T:System.Guid"></see> that has the same value as this instance;
+        /// true if <paramref name="o">o</paramref> is a string that has the same value as this instance;
+        /// otherwise, false.
+        /// </returns>
+        public override bool Equals(object o)
+        {
+            switch (o)
+            {
+                case ShortGuid shortGuid:
+                    return _guid.Equals(shortGuid._guid);
+
+                case Guid guid:
+                    return _guid.Equals(guid);
+
+                case string @string:
+                    return _value.Equals(@string);
+
+                default:
+                    return false;
+            }
+        }
+
+        #endregion
+
+        #region GetHashCode
+
+        // ReSharper disable NonReadonlyMemberInGetHashCode
+        /// <summary>
+        /// Returns the HashCode of underlying Guid.
+        /// </summary>
+        /// <returns>HashCode of Guid</returns>
+        public override int GetHashCode()
+            => _guid.GetHashCode();
+
+        #endregion
+
+        #region NewShortGuid
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:ShortGuid"></see> structure.
+        /// </summary>
+        /// <returns>A new ShortGuid object.</returns>
+        public static ShortGuid NewShortGuid()
+            => new ShortGuid(Guid.NewGuid());
+
+        #endregion
+
+        #region == operator
+
+        /// <summary>
+        /// Indicates whether the values of two specified <see cref="T:ShortGuid"></see> objects are equal.
+        /// </summary>
+        /// <param name="a">The first object to compare.</param>
+        /// <param name="b">The second object to compare.</param>
+        /// <returns>true if <paramref name="a">a</paramref> and <paramref name="b">b</paramref> are equal; otherwise, false.</returns>
+        public static bool operator ==(ShortGuid a, ShortGuid b)
+            => a._guid == b._guid;
+
+        #endregion
+
+        #region != operator
+
+        /// <summary>
+        /// Indicates whether the values of two specified <see cref="T:ShortGuid"></see> objects are not equal.
+        /// </summary>
+        /// <param name="a">The first object to compare.</param>
+        /// <param name="b">The second object to compare.</param>
+        /// <returns>true if <paramref name="a">a</paramref> and <paramref name="b">b</paramref> are not equal; otherwise, false.</returns>
+        public static bool operator !=(ShortGuid a, ShortGuid b)
+            => !(a == b);
+
+        #endregion
+
+        #region Encode
+
+        /// <summary>
+        /// Creates a new instance of a <see cref="T:System.Guid"></see> using the string value, 
+        /// then returns the base64 encoded version of the <see cref="T:System.Guid"></see>.
+        /// </summary>
+        /// <param name="value">An actual <see cref="T:System.Guid">Guid</see> string.</param>
+        /// <returns>Base64 version of <see cref="T:System.Guid"></see></returns>
+        private static string Encode(string value)
+        {
+            var guid = new Guid(value);
+            return Encode(guid);
+        }
+
+        /// <summary>
+        /// Creates a new instance of a <see cref="T:System.Guid"></see> using the
+        /// <see cref="T:System.Guid">Guid</see>.
+        /// </summary>
+        /// <param name="guid">The <see cref="T:System.Guid">Guid</see> to encode</param>
+        /// <returns>Base64 version of <see cref="T:System.Guid"></see></returns>
+        private static string Encode(Guid guid)
+        {
+            string encoded = Convert.ToBase64String(guid.ToByteArray());
+            encoded = encoded
+                .Replace("/", "_")
+                .Replace("+", "-");
+            return encoded.Substring(0, 22);
+        }
+
+        #endregion
+
+        #region Decode
+
+        /// <summary>
+        /// Decodes the given base64 string to <see cref="T:System.Guid"></see>.
+        /// </summary>
+        /// <param name="s">The base64 encoded string of a <see cref="T:System.Guid">Guid</see>.</param>
+        /// <exception cref="T:System.ArgumentNullException"><paramref name="s">s</paramref> is null.</exception>
+        /// <exception cref="T:System.FormatException">The length of <paramref name="s">s</paramref>, ignoring white-space characters, is not zero or a multiple of 4.   -or-   The format of <paramref name="s">s</paramref> is invalid. <paramref name="s">s</paramref> contains a non-base-64 character, more than two padding characters, or a non-white space-character among the padding characters.</exception>
+        /// <returns>A new <see cref="T:System.Guid">Guid</see>.</returns>
+        private static Guid Decode(string s)
+        {
+            if (s == null)
+                throw new ArgumentNullException(nameof(s), "Value cannot be null.");
+
+            s = s
+                .Replace("_", "/")
+                .Replace("-", "+");
+            var buffer = Convert.FromBase64String(s + "==");
+            return new Guid(buffer);
+        }
+
+        #endregion
+
+        #region ToString
+
+        /// <summary>
+        /// Returns the base64 encoded <see cref="T:ShortGuid">ShortGuid</see> as a string.
+        /// </summary>
+        /// <returns>The value of <see cref="T:ShortGuid"></see>.</returns>
+        public override string ToString()
+            => _value;
+
+        #endregion
+
+        #region ShortGuid operator 
+
+        /// <summary>
+        /// Implicitly converts the <see cref="T:System.Guid">Guid</see>
+        /// to a <see cref="T:ShortGuid">ShortGuid</see>.
+        /// </summary>
+        /// <param name="guid">The guid value of <see cref="T:ShortGuid">ShortGuid</see>.</param>
+        /// <returns>A new <see cref="T:ShortGuid">ShortGuid</see> object.</returns>
+        public static implicit operator ShortGuid(Guid guid)
+            => new ShortGuid(guid);
+
+        /// <summary>
+        /// Implicitly converts the string to a <see cref="T:ShortGuid">ShortGuid</see>.
+        /// </summary>
+        /// <param name="value">The string value of <see cref="T:ShortGuid">ShortGuid</see>.</param>
+        /// <returns>A new <see cref="T:ShortGuid">ShortGuid</see> object.</returns>
+        public static implicit operator ShortGuid(string value)
+            => new ShortGuid(value);
+
+        #endregion
+
+        #region Guid operator
+
+        /// <summary>
+        /// Implicitly converts the <see cref="T:ShortGuid">ShortGuid</see>
+        /// to it's <see cref="T:System.Guid">Guid</see> equivalent.
+        /// </summary>
+        /// <param name="shortGuid"><see cref="T:ShortGuid">ShortGuid</see> to convert.</param>
+        /// <returns><see cref="T:System.Guid">Guid</see> equivalent of <see cref="T:ShortGuid">ShortGuid</see>.</returns>
+        public static implicit operator Guid(ShortGuid shortGuid)
+            => shortGuid._guid;
+
+        #endregion
+
+        #region string operator
+
+        /// <summary>
+        /// Implicitly converts the <see cref="T:ShortGuid">ShortGuid</see>
+        /// to it's string equivalent.
+        /// </summary>
+        /// <param name="shortGuid"><see cref="T:ShortGuid">ShortGuid</see> to convert.</param>
+        /// <returns>String equivalent of <see cref="T:ShortGuid">ShortGuid</see>.</returns>
+        public static implicit operator string(ShortGuid shortGuid)
+            => shortGuid._value;
+
+        #endregion
+    }
+}
---
+++ tests/Mariowski.Common.UnitTests/DataTypes/ShortGuidTests.cs
@@ -0,0 +1,144 @@
+﻿using FluentAssertions;
+using Mariowski.Common.DataTypes;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+// ReSharper disable EqualExpressionComparison
+// ReSharper disable SuspiciousTypeConversion.Global
+
+namespace Mariowski.Common.UnitTests.DataTypes
+{
+    public class ShortGuidTests
+    {
+        [Fact]
+        public void ShortGuid_Empty_ShouldBeTheSameAsDefaultValue()
+        {
+            var defaultGuid = default(ShortGuid);
+
+            (ShortGuid.Empty.Guid == defaultGuid.Guid).Should().BeTrue();
+            (ShortGuid.Empty.Value == defaultGuid.Value).Should().BeTrue();
+            (ShortGuid.Empty == defaultGuid).Should().BeTrue();
+        }
+
+        [Fact]
+        public void ShortGuid_Empty_ShouldBeDifferentFromGeneratedShortGuid()
+        {
+            var shortGuid = ShortGuid.NewShortGuid();
+
+            (ShortGuid.Empty.Guid != shortGuid.Guid).Should().BeTrue();
+            (ShortGuid.Empty.Value != shortGuid.Value).Should().BeTrue();
+            (ShortGuid.Empty != shortGuid).Should().BeTrue();
+        }
+
+        [Fact]
+        public void ShortGuid_Equals_ShouldCorrectlyDetermineEqualityWithOtherObject()
+        {
+            var guid = new Guid("9a717c34-ed19-4446-8752-ccd020ed9638");
+            var shortGuid = new ShortGuid(guid);
+            var shortGuid2 = new ShortGuid(guid);
+
+            shortGuid.Equals(shortGuid).Should().BeTrue();
+            shortGuid.Equals(shortGuid2).Should().BeTrue();
+            shortGuid.Equals(guid).Should().BeTrue();
+            shortGuid.Equals(shortGuid2.Value).Should().BeTrue();
+            shortGuid.Equals(123).Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShortGuid_GetHashCode_ShouldReturnTheSameHashCodeAsGuid()
+        {
+            var shortGuid = ShortGuid.NewShortGuid();
+
+            (shortGuid.GetHashCode() == shortGuid.Guid.GetHashCode()).Should().BeTrue();
+        }
+
+        [Fact]
+        public void ShortGuid_NewShortGuid_ShouldReturnUniqueShortGuids()
+        {
+            const int guidsToGenerate = 10000;
+            var shortGuids = new List<ShortGuid>(guidsToGenerate);
+
+            for (int i = 0; i < guidsToGenerate; i++)
+            {
+                var shortGuid = ShortGuid.NewShortGuid();
+                shortGuids.Exists(g => g == shortGuid).Should().BeFalse();
+                shortGuids.Add(shortGuid);
+            }
+        }
+
+        [Fact]
+        public void ShortGuid_EqualityOperator_ShouldCorrectlyDetermineEquality()
+        {
+            var shortGuid = ShortGuid.NewShortGuid();
+            var shortGuidCopy = shortGuid;
+            var shortGuid2 = ShortGuid.NewShortGuid();
+
+            (shortGuid.Guid == shortGuidCopy.Guid).Should().BeTrue();
+            (shortGuid.Value == shortGuidCopy.Value).Should().BeTrue();
+            (shortGuid == shortGuidCopy).Should().BeTrue();
+
+            (shortGuid.Guid == shortGuid2.Guid).Should().BeFalse();
+            (shortGuid.Value == shortGuid2.Value).Should().BeFalse();
+            (shortGuid == shortGuid2).Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShortGuid_InequalityOperator_ShouldCorrectlyDetermineInequality()
+        {
+            var shortGuid = ShortGuid.NewShortGuid();
+            var shortGuidCopy = shortGuid;
+            var shortGuid2 = ShortGuid.NewShortGuid();
+
+            (shortGuid.Guid != shortGuid2.Guid).Should().BeTrue();
+            (shortGuid.Value != shortGuid2.Value).Should().BeTrue();
+            (shortGuid != shortGuid2).Should().BeTrue();
+
+            (shortGuid.Guid != shortGuidCopy.Guid).Should().BeFalse();
+            (shortGuid.Value != shortGuidCopy.Value).Should().BeFalse();
+            (shortGuid != shortGuidCopy).Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShortGuid_ToString_ShouldReturnValueOfShortGuid()
+        {
+            var shortGuid = ShortGuid.NewShortGuid();
+
+            (shortGuid.ToString() == shortGuid.Value).Should().BeTrue();
+        }
+
+        [Fact]
+        public void ShortGuid_ShortGuidOperator_ShouldImplicitlyConvertsGuidToShortGuid()
+        {
+            ShortGuid shortGuid = Guid.NewGuid();
+
+            shortGuid.Should().NotBeNull();
+            shortGuid.Should().NotBeSameAs(ShortGuid.Empty);
+        }
+
+        [Fact]
+        public void ShortGuid_ShortGuidOperator_ShouldImplicitlyConvertsStringToShortGuid()
+        {
+            ShortGuid shortGuid = "eRy4yaDYjkuYob4YnHKtnA";
+
+            shortGuid.Should().NotBeNull();
+            shortGuid.Should().NotBeSameAs(ShortGuid.Empty);
+        }
+
+        [Fact]
+        public void ShortGuid_GuidOperator_ShouldImplicitlyConvertsShortGuidToGuid()
+        {
+            Guid guid = new ShortGuid("eRy4yaDYjkuYob4YnHKtnA");
+
+            guid.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public void ShortGuid_StringOperator_ShouldImplicitlyConvertsShortGuidToString()
+        {
+            string value = new ShortGuid(new Guid("9a717c34-ed19-4446-8752-ccd020ed9638"));
+
+            value.Should().NotBeNullOrWhiteSpace();
+        }
+    }
+}
