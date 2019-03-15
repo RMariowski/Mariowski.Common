cc0d92ab666d59e2d1237901440a4d1a395a03ad
Radek Mariowski <rmariowski@gmail.com>
2019-03-15 16:37:42 +0100

    TimestampableEntity

---
+++ src/Mariowski.Common.DataSource/Entities/TimestampableEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Mariowski.Common.DataSource.Entities
+{
+    [Serializable]
+    public abstract class TimestampableEntity<TPrimaryKey> : Entity<TPrimaryKey>, ITimestampable
+    {
+        #region Fields
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates new instance of <see cref="T:TimestampableEntity"/> with dates set to <see cref="P:DateTime.UtcNow"/>.
+        /// </summary>
+        protected TimestampableEntity()
+        {
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = CreatedAt;
+        }
+
+        /// <summary>
+        /// Creates new instance of <see cref="T:TimestampableEntity"/> with specified <see cref="T:DateTime"/>.
+        /// </summary>
+        /// <param name="createdAt">Entity creation date.</param>
+        /// <param name="updatedAt">Entity last update date.</param>
+        protected TimestampableEntity(DateTime createdAt, DateTime? updatedAt = null)
+        {
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt ?? CreatedAt;
+        }
+
+        #endregion
+    }
+}
---
+++ tests/Mariowski.Common.DataSource.UnitTests/Entities/TimestampableEntityTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Mariowski.Common.DataSource.Entities;
+using System;
+using Xunit;
+
+namespace Mariowski.Common.DataSource.UnitTests.Entities
+{
+    public class TimestampableEntityTests
+    {
+        private class ClassToTest : TimestampableEntity<int>
+        {
+            public ClassToTest() { }
+            public ClassToTest(DateTime createdAt, DateTime? updatedAt = null) : base(createdAt, updatedAt) { }
+        }
+        
+        [Fact]
+        public void TimestampableEntity_Ctor_CreatingInstanceWithoutArgumentsShouldHaveCorrectDates()
+        {
+            var entity = new ClassToTest();
+            entity.CreatedAt.Should().BeIn(DateTimeKind.Utc).And.BeCloseTo(DateTime.UtcNow, 500);
+            entity.UpdatedAt.Should().BeSameDateAs(entity.CreatedAt);
+        }
+
+        [Fact]
+        public void TimestampableEntity_Ctor_CreatingInstanceWithArgumentsShouldHaveCorrectDates()
+        {
+            var createdAt = DateTime.UtcNow;
+            var entity = new ClassToTest(createdAt);
+            entity.CreatedAt.Should().BeIn(DateTimeKind.Utc).And.BeCloseTo(DateTime.UtcNow, 500);
+            entity.UpdatedAt.Should().BeSameDateAs(entity.CreatedAt);
+
+            var updatedAt = DateTime.UtcNow.AddDays(1);
+            entity = new ClassToTest(createdAt, updatedAt);
+            entity.CreatedAt.Should().BeIn(DateTimeKind.Utc).And.BeCloseTo(DateTime.UtcNow, 500);
+            entity.UpdatedAt.Should().NotBeSameDateAs(entity.CreatedAt);
+            entity.UpdatedAt.Should().BeIn(DateTimeKind.Utc).And.BeCloseTo(DateTime.UtcNow.AddDays(1), 500);
+        }
+    }
+}
