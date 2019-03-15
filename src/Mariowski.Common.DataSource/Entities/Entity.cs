66eec3db2c417f8c0103a9fc551fe6ad596aef5f
Radek Mariowski <rmariowski@gmail.com>
2019-03-15 16:36:37 +0100

    Entity

---
+++ src/Mariowski.Common.DataSource/Entities/Entity.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mariowski.Common.DataSource.Entities
+{
+    [Serializable]
+    public abstract class Entity<TPrimaryKey> : IEntity<TPrimaryKey>
+    {
+        #region Fields
+
+        public virtual TPrimaryKey Id { get; set; }
+
+        #endregion
+
+        #region IsTransient
+
+        /// <inheritdoc />
+        public virtual bool IsTransient()
+        {
+            if (EqualityComparer<TPrimaryKey>.Default.Equals(Id, default(TPrimaryKey)))
+                return true;
+
+            // Workaround for EF Core since it sets int/long to min value when attaching to db context.
+            var typeOfPrimaryKey = typeof(TPrimaryKey);
+            if (typeOfPrimaryKey == typeof(int))
+                return Convert.ToInt32(Id) <= 0;
+
+            if (typeOfPrimaryKey == typeof(long))
+                return Convert.ToInt64(Id) <= 0;
+
+            return false;
+        }
+
+        #endregion
+
+        #region Equals
+
+        /// <summary>
+        /// Returns a value that indicates whether this instance is equal to a specified object.
+        /// </summary>
+        /// <param name="obj">The object to compare with this instance.</param>
+        /// <returns>
+        /// True if <paramref name="obj">obj</paramref> is a same instance as this instance;
+        /// True if <paramref name="obj">obj</paramref> is a <see cref="T:Entity{TPrimaryKey}"></see> that has the same Id as this instance;
+        /// Otherwise, false.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Entity<TPrimaryKey>))
+                return false;
+
+            // Same instances must be considered as equal.
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            // Transient objects are not considered as equal.
+            var other = (Entity<TPrimaryKey>)obj;
+            if (IsTransient() && other.IsTransient())
+                return false;
+
+            // Must have a IS-A relation of types or must be same type.
+            var typeOfThis = GetType();
+            var typeOfOther = other.GetType();
+            if (!typeOfThis.IsAssignableFrom(typeOfOther) && !typeOfOther.IsAssignableFrom(typeOfThis))
+                return false;
+
+            return Id.Equals(other.Id);
+        }
+
+        #endregion
+
+        #region GetHashCode
+
+        // ReSharper disable NonReadonlyMemberInGetHashCode
+        /// <summary>
+        /// Returns the HashCode of underlying Id.
+        /// </summary>
+        /// <returns>HashCode of Id.</returns>
+        public override int GetHashCode()
+            => Id.GetHashCode();
+
+        #endregion
+
+        #region == operator
+
+        /// <summary>
+        /// Indicates whether the values of two specified <see cref="T:Entity{TPrimaryKey}"></see> objects are equal.
+        /// </summary>
+        /// <param name="left">The first object to compare.</param>
+        /// <param name="right">The second object to compare.</param>
+        /// <returns>True if <paramref name="left">left</paramref> and <paramref name="right">right</paramref> are equal; otherwise, false.</returns>
+        public static bool operator ==(Entity<TPrimaryKey> left, Entity<TPrimaryKey> right)
+            => left?.Equals(right) ?? Equals(right, null);
+
+        #endregion
+
+        #region != operator
+
+        /// <summary>
+        /// Indicates whether the values of two specified <see cref="T:Entity{TPrimaryKey}"></see> objects are not equal.
+        /// </summary>
+        /// <param name="left">The first object to compare.</param>
+        /// <param name="right">The second object to compare.</param>
+        /// <returns>true if <paramref name="left">left</paramref> and <paramref name="right">right</paramref> are not equal; otherwise, false.</returns>
+        public static bool operator !=(Entity<TPrimaryKey> left, Entity<TPrimaryKey> right)
+            => !(left == right);
+
+        #endregion
+
+        #region ToString
+
+        /// <summary>
+        /// Returns the string in following format: "[{TypeName} {Id}]".
+        /// </summary>
+        /// <returns>The string in following format: "[{TypeName} {Id}]".</returns>
+        public override string ToString()
+            => $"[{GetType().Name} {Id}]";
+
+        #endregion
+    }
+}
---
+++ tests/Mariowski.Common.DataSource.UnitTests/Entities/EntityTests.cs
@@ -0,0 +1,94 @@
+﻿using FluentAssertions;
+using Mariowski.Common.DataSource.Entities;
+using Xunit;
+
+// ReSharper disable ConditionIsAlwaysTrueOrFalse
+// ReSharper disable EqualExpressionComparison
+// ReSharper disable SuspiciousTypeConversion.Global
+
+namespace Mariowski.Common.DataSource.UnitTests.Entities
+{
+    public class EntityTests
+    {
+        private class ClassToTest : Entity<int> { }
+
+        [Fact]
+        public void Entity_IsTransient_EntityWithoutIdShouldBeTransient()
+        {
+            var entity = new ClassToTest();
+            entity.IsTransient().Should().BeTrue();
+        }
+
+        [Fact]
+        public void Entity_IsTransient_EntityWithDefaultIdShouldBeTransient()
+        {
+            var entity = new ClassToTest { Id = 0 };
+            entity.IsTransient().Should().BeTrue();
+        }
+
+        [Fact]
+        public void Entity_IsTransient_EntityWithCorrectIdShouldNotBeTransient()
+        {
+            var entity = new ClassToTest { Id = 1 };
+            entity.IsTransient().Should().BeFalse();
+        }
+
+        [Fact]
+        public void Entity_Equals_ShouldCorrectlyDetermineEqualityWithOtherObject()
+        {
+            var entity = new ClassToTest();
+            entity.Equals(entity).Should().BeTrue();
+            entity.Equals(null).Should().BeFalse();
+            entity.Equals("").Should().BeFalse();
+            entity.Equals(123).Should().BeFalse();
+
+            var entity2 = new ClassToTest();
+            entity.Equals(entity2).Should().BeFalse();
+
+            entity.Id = entity2.Id = 1;
+            entity.Equals(entity2).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Entity_GetHashCode_ShouldReturnTheSameHashCodeAsId()
+        {
+            var entity = new ClassToTest { Id = 1 };
+            (entity.Id.GetHashCode() == 1.GetHashCode()).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Entity_EqualityOperator_ShouldCorrectlyDetermineEquality()
+        {
+            var entity = new ClassToTest();
+            (entity == entity).Should().BeTrue();
+            (entity == null).Should().BeFalse();
+
+            var entity2 = new ClassToTest();
+            (entity == entity2).Should().BeFalse();
+
+            entity.Id = entity2.Id = 1;
+            (entity == entity2).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Entity_InequalityOperator_ShouldCorrectlyDetermineInequality()
+        {
+            var entity = new ClassToTest();
+            (entity != entity).Should().BeFalse();
+            (entity != null).Should().BeTrue();
+
+            var entity2 = new ClassToTest();
+            (entity != entity2).Should().BeTrue();
+
+            entity.Id = entity2.Id = 1;
+            (entity != entity2).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Entity_ToString_ShouldReturnStringInSpecifiedFormat()
+        {
+            var entity = new ClassToTest();
+            entity.ToString().Should().Be($"[{typeof(ClassToTest).Name} {entity.Id}]");
+        }
+    }
+}
