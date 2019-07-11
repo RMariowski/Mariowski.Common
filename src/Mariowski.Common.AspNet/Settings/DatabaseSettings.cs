e24e1dfe450d440f0188e029e6e6fd74e9ba90b5
Radek Mariowski <rmariowski@gmail.com>
2019-07-11 18:31:42 +0200

    Added AspNet project

---
+++ src/Mariowski.Common.AspNet/Settings/DatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿using Mariowski.Common.Markers;
+
+namespace Mariowski.Common.AspNet.Settings
+{
+    public class DatabaseSettings : ISettings
+    {
+        public const string SectionName = "Database";
+        
+        public string ConnectionString { get; set; }
+        public bool InMemory { get; set; }
+    }
+}
---
+++ src/Mariowski.Common.AspNet/Settings/SmtpSettings.cs
@@ -0,0 +1,16 @@
+﻿using Mariowski.Common.Markers;
+
+namespace Mariowski.Common.AspNet.Settings
+{
+    public class SmtpSettings : ISettings
+    {
+        public const string SectionName = "SMTP";
+        
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool EnableSsl { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string From { get; set; }
+    }
+}
