3d834aee5ffb4e10bed8f8b04a9f98f545b0447e
Radek Mariowski <rmariowski@gmail.com>
2020-06-30 18:46:42 +0200

    Refactor list extensions tests

--- tests/Mariowski.Common.UnitTests/Extensions/ListExtensionsTests.cs
+++ tests/Mariowski.Common.UnitTests/Extensions/ListExtensionsTests.cs
@@ -9,23 +9,25 @@ namespace Mariowski.Common.UnitTests.Extensions
     public class ListExtensionsTests
     {
         [Fact]
-        public void ListExtensions_Shuffle_ShouldShuffleList()
+        public void Shuffle_ShouldShuffleList()
         {
             var originalList = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
             var list = new List<int>(originalList);
 
             list.Shuffle();
+
             list.Should().NotEqual(originalList, "chance of getting the same order should be small enough.");
         }
 
         [Fact]
-        public void ListExtensions_Shuffle_ShouldShuffleTwoElementsList()
+        public void Shuffle_ShouldShuffleTwoElementsList()
         {
             var random = new Random(1);
-            var originalList = new List<int> { 0, 1};
+            var originalList = new List<int> { 0, 1 };
             var list = new List<int>(originalList);
 
             list.Shuffle(random);
+
             list.Should().NotEqual(originalList);
         }
     }
