a5868867e69a761e8a436e751497ea0004a85b22
Radek Mariowski <rmariowski@gmail.com>
2020-07-04 17:24:09 +0200

    Add ToJsonContent extension for object

---
+++ src/Mariowski.Common.AspNet/Extensions/ObjectExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace Mariowski.Common.AspNet.Extensions
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Serializes object to JSON, then converts to <see cref="T:StringContent"></see>.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="encoding">The encoding to use for the content.</param>
+        /// <returns>Object as string content.</returns>
+        public static StringContent ToJsonContent(this object @this, Encoding encoding = null)
+        {
+            string json = JsonSerializer.Serialize(@this);
+            return new StringContent(json, encoding ?? Encoding.UTF8, "application/json");
+        }
+    }
+}
