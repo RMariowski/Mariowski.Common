425e93f7607d3e1f896c50b6631333c637df0a2c
Radek Mariowski <rmariowski@gmail.com>
2020-07-13 21:01:38 +0200

    Change argument name of ToUnixTimestamp extension

--- src/Mariowski.Common/Extensions/DateTimeExtensions.cs
+++ src/Mariowski.Common/Extensions/DateTimeExtensions.cs
@@ -7,9 +7,9 @@ namespace Mariowski.Common.Extensions
         /// <summary>
         /// Gets unix timestamp in seconds based on date time.
         /// </summary>
-        /// <param name="this">The @this to act on.</param>
+        /// <param name="dateTime">The date time to act on.</param>
         /// <returns>Unix timestamp in seconds.</returns>
-        public static long ToUnixTimestamp(this DateTime @this)
-            => ((DateTimeOffset)@this).ToUnixTimeSeconds();
+        public static long ToUnixTimestamp(this DateTime dateTime)
+            => ((DateTimeOffset)dateTime).ToUnixTimeSeconds();
     }
 }
