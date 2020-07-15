df7b58f8144bcf4c585498aa26d3bbf0f802b7a1
Radek Mariowski <rmariowski@gmail.com>
2020-07-15 20:40:31 +0200

    Change argument name of ToJsonContent() from @this to obj

--- src/Mariowski.Common.AspNet/Extensions/ObjectExtensions.cs
+++ src/Mariowski.Common.AspNet/Extensions/ObjectExtensions.cs
@@ -9,12 +9,12 @@ namespace Mariowski.Common.AspNet.Extensions
         /// <summary>
         /// Serializes object to JSON, then converts to <see cref="T:StringContent"></see>.
         /// </summary>
-        /// <param name="this">The @this to act on.</param>
+        /// <param name="obj">The object to act on.</param>
         /// <param name="encoding">The encoding to use for the content.</param>
         /// <returns>Object as string content.</returns>
-        public static StringContent ToJsonContent(this object @this, Encoding encoding = null)
+        public static StringContent ToJsonContent(this object obj, Encoding encoding = null)
         {
-            string json = JsonSerializer.Serialize(@this);
+            string json = JsonSerializer.Serialize(obj);
             return new StringContent(json, encoding ?? Encoding.UTF8, "application/json");
         }
     }
