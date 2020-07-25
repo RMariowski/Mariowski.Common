8f12969c688ec05b45a7f2ba4a804222dd7dc5a0
Radek Mariowski <rmariowski@gmail.com>
2020-07-25 17:55:25 +0200

    Add some unit tests for EfGenericRepository

---
+++ tests/Mariowski.Common.EntityFramework.UnitTests/EfGenericRepositoryTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Mariowski.Common.EntityFramework.UnitTests
+{
+    public class EfGenericRepositoryTests
+    {
+        [Fact]
+        public async Task InsertAndGetIdAsync_ShouldReturnIdOfInsertedEntity()
+        {
+            await using var context = new TestContext();
+            var dummyRepository = new DummyRepository(context);
+
+            int id = await dummyRepository.InsertAndGetIdAsync(new DummyEntity { Foo = "Insert" });
+
+            id.Should().Be(1);
+        }
+
+        [Fact]
+        public async Task Should_delete_updated_entity()
+        {
+            await using var context = new TestContext();
+            var dummyRepository = new DummyRepository(context);
+
+            await dummyRepository.InsertAsync(new DummyEntity { Foo = "Insert" });
+            await context.SaveChangesAsync();
+
+            var entity = await dummyRepository.SingleAsync(dummy => dummy.Foo == "Insert");
+            entity.Foo = "Update";
+            await dummyRepository.UpdateAsync(entity);
+            await context.SaveChangesAsync();
+
+            entity = await dummyRepository.SingleAsync(dummy => dummy.Foo == "Update");
+            await dummyRepository.DeleteAsync(entity);
+            await context.SaveChangesAsync();
+
+            bool deleted = !await dummyRepository.AnyAsync(x => true);
+            deleted.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Should_update_entity_twice()
+        {
+            await using var context = new TestContext();
+            var dummyRepository = new DummyRepository(context);
+
+            await dummyRepository.InsertAsync(new DummyEntity { Foo = "Insert" });
+            await context.SaveChangesAsync();
+
+            var entity = await dummyRepository.SingleAsync(dummy => dummy.Foo == "Insert");
+            entity.Foo = "Update1";
+            await dummyRepository.UpdateAsync(entity);
+            await context.SaveChangesAsync();
+
+            entity = await dummyRepository.SingleAsync(dummy => dummy.Foo == "Update1");
+            entity.Foo = "Update2";
+            await dummyRepository.UpdateAsync(entity);
+            await context.SaveChangesAsync();
+
+            entity = await dummyRepository.SingleAsync(dummy => dummy.Foo == "Update2");
+            entity.Should().NotBeNull();
+        }
+    }
+}
